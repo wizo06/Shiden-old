2e6ce2c53c2962c980afbf7ac1289e4d24e722a2
wizo06 <14183421+wizo06@users.noreply.github.com>
2020-02-03 04:46:32 -0500

    Refactored POST /encode and POST /batch

---
+++ src/pipeline.js
@@ -0,0 +1,50 @@
+// Import node modules
+const Discord = require('discord.js');
+const DISCORD_BOT = new Discord.Client();
+
+// Import custom modules
+const Encoder = require(path.join(process.cwd(), 'src/automata/encoder.js'));
+const Logger = require(path.join(process.cwd(), 'src/utils/logger.js'));
+const Notification = require(path.join(process.cwd(), 'src/automata/notification.js'));
+const Rclone = require(path.join(process.cwd(), 'src/automata/rclone.js'));
+const Queue = require(path.join(process.cwd(), 'src/utils/queue.js'));
+const Temp = require(path.join(process.cwd(), 'src/utils/temp.js'));
+
+const CONFIG = require(path.join(process.cwd(), 'src/utils/config.js'));
+
+module.exports = processNextPayload = async () => {
+  try {
+    Logger.info(`[0/4] Processing next payload in queue`, Logger.Colors.Bright + Logger.Colors.FgCyan);
+    console.log(util.inspect(await Queue.getFirst()));
+    Logger.info('[1/4] Downloading episode file...', Logger.Colors.FgCyan);
+    if (CONFIG.discord_bot.token) await DISCORD_BOT.user.setActivity('download', { type: 'PLAYING' });
+    await Rclone.download();
+    Logger.info('[2/4] Hardsubbing episode file...', Logger.Colors.FgCyan);
+    if (CONFIG.discord_bot.token) await DISCORD_BOT.user.setActivity('hardsub', { type: 'PLAYING' });
+    await Encoder.encode();
+    Logger.info('[3/4] Uploading episode file...', Logger.Colors.FgCyan);
+    if (CONFIG.discord_bot.token) await DISCORD_BOT.user.setActivity('upload', { type: 'PLAYING' });
+    await Rclone.upload();
+    Logger.info('[4/4] Sending notifications...', Logger.Colors.FgCyan);
+    Notification.notification();
+    await Temp.destroy();
+    await Queue.shift();
+    Logger.info(`Job completed`, Logger.Colors.FgGreen);
+    if (!(await Queue.isEmpty())) processNextPayload();
+    if (CONFIG.discord_bot.token) await DISCORD_BOT.user.setActivity('idle', { type: 'PLAYING' });
+  }
+  catch (status) {
+    Logger.error(`Exit code: ${status}`);
+    Logger.info('[4/4] Sending notifications...', Logger.Colors.FgCyan);
+    try {
+      Notification.notification(status);
+      await Temp.destroy();
+      await Queue.shift();
+      if (!(await Queue.isEmpty())) processNextPayload();
+      if (CONFIG.discord_bot.token) await DISCORD_BOT.user.setActivity('idle', { type: 'PLAYING' });
+    }
+    catch (e) {
+      Logger.error(e);
+    }
+  }
+};
---
+++ src/routes/postEncode.js
@@ -0,0 +1,37 @@
+// Import node modules
+const path = require('path');
+const express = require('express');
+const router = new express.Router();
+
+// Import custom modules
+const Auth = require(path.join(process.cwd(), 'src/utils/auth.js'));
+const Logger = require(path.join(process.cwd(), 'src/utils/logger.js'));
+const Queue = require(path.join(process.cwd(), 'src/utils/queue.js'));
+
+const { processNextPayload } = require(path.join(process.cwd(), 'src/pipeline.js'));
+
+module.exports = router.post('/encode', async (req, res) => {
+  try {
+    if (!Auth.authorize(req.get('Authorization'))) return res.status(401).send('Not authorized');
+    if (req.get('Content-Type') !== 'application/json') return res.status(415).send('Content-Type must be application/json');
+    if (!(req.body.show && req.body.full_path)) return res.status(400).send('JSON body must have "show" and "full_path"');
+    if (await Queue.includes(req.body)) return res.status(409).send('Payload is in queue already');
+
+    res.status(209).send('Payload accepted');
+
+    const payload = req.body;
+    Logger.info(`Loaded show: ${payload.show}`, Logger.Colors.Bright + Logger.Colors.FgMagenta);
+    Logger.info(`Loaded episode: ${path.basename(payload.full_path)}`, Logger.Colors.Bright + Logger.Colors.FgMagenta);
+
+    if (await Queue.isEmpty()) {
+      await Queue.push(payload);
+      processNextPayload();
+    }
+    else {
+      await Queue.push(payload);
+    }
+  }
+  catch (e) {
+    Logger.error(e);
+  }
+});
